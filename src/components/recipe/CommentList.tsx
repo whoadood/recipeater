e853baffa7a9e15eaa7adaeb98a323af3c9e5340
Castle <castlerock88@protonmail.com>
2022-10-23 19:40:10 -0400

    feat: comments section styles

--- src/components/recipe/CommentList.tsx
+++ src/components/recipe/CommentList.tsx
@@ -1,8 +1,11 @@
 // Packages
 import { QuestionMarkCircleIcon } from "@heroicons/react/20/solid";
+import { Field, Form, Formik } from "formik";
+import { useSession } from "next-auth/react";
 
 // Hooks
 import { useDarkmode } from "../../hooks/useDark";
+import ErrMessage from "./createForm/ErrMessage";
 
 const comments = [
   {
@@ -37,12 +40,22 @@ const user = {
 
 export default function CommentList() {
   const { darkmode } = useDarkmode();
+  const { data: session } = useSession();
   return (
     <section aria-labelledby="notes-title">
-      <div className="bg-white shadow sm:overflow-hidden sm:rounded-lg">
+      <div
+        className={`${
+          darkmode ? "bg-[#2e2e2e]" : "bg-inherit"
+        } shadow sm:overflow-hidden sm:rounded-lg`}
+      >
         <div className="divide-y divide-gray-200">
           <div className="px-4 py-5 sm:px-6">
-            <h2 id="notes-title" className="text-lg font-medium text-gray-900">
+            <h2
+              id="notes-title"
+              className={`text-lg font-medium ${
+                darkmode ? "text-white" : "text-gray-900"
+              }`}
+            >
               Notes
             </h2>
           </div>
@@ -60,26 +73,22 @@ export default function CommentList() {
                     </div>
                     <div>
                       <div className="text-sm">
-                        <a href="#" className="font-medium text-gray-900">
+                        <a
+                          href="#"
+                          className={`font-medium ${
+                            darkmode ? "text-white" : "text-gray-900"
+                          }`}
+                        >
                           {comment.name}
                         </a>
                       </div>
-                      <div className="mt-1 text-sm text-gray-700">
+                      <div className="mt-1 text-sm text-gray-400">
                         <p>{comment.body}</p>
                       </div>
                       <div className="mt-2 space-x-2 text-sm">
                         <span className="font-medium text-gray-500">
                           {comment.date}
                         </span>{" "}
-                        <span className="font-medium text-gray-500">
-                          &middot;
-                        </span>{" "}
-                        <button
-                          type="button"
-                          className="font-medium text-gray-900"
-                        >
-                          Reply
-                        </button>
                       </div>
                     </div>
                   </div>
@@ -88,52 +97,78 @@ export default function CommentList() {
             </ul>
           </div>
         </div>
-        <div className="bg-gray-50 px-4 py-6 sm:px-6">
-          <div className="flex space-x-3">
-            <div className="flex-shrink-0">
-              <img
-                className="h-10 w-10 rounded-full"
-                src={user.imageUrl}
-                alt=""
-              />
-            </div>
-            <div className="min-w-0 flex-1">
-              <form action="#">
-                <div>
-                  <label htmlFor="comment" className="sr-only">
-                    About
-                  </label>
-                  <textarea
-                    id="comment"
-                    name="comment"
-                    rows={3}
-                    className="block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
-                    placeholder="Add a note"
-                    defaultValue={""}
-                  />
-                </div>
-                <div className="mt-3 flex items-center justify-between">
-                  <a
-                    href="#"
-                    className="group inline-flex items-start space-x-2 text-sm text-gray-500 hover:text-gray-900"
-                  >
-                    <QuestionMarkCircleIcon
-                      className="h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
-                      aria-hidden="true"
-                    />
-                    <span>Some HTML is okay.</span>
-                  </a>
-                  <button
-                    type="submit"
-                    className="inline-flex items-center justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
-                  >
-                    Comment
-                  </button>
-                </div>
-              </form>
+        {session && (
+          <div
+            className={`${
+              darkmode ? "bg-[#3e3e3e]" : "bg-gray-50"
+            } px-4 py-6 sm:px-6`}
+          >
+            <div className="flex space-x-3">
+              <div className="flex-shrink-0">
+                <img
+                  className="h-10 w-10 rounded-full"
+                  src={session.user?.image as string}
+                  alt=""
+                />
+              </div>
+              <div className="min-w-0 flex-1">
+                <Formik
+                  initialValues={{ text: "" }}
+                  onSubmit={(values, { resetForm }) => {
+                    console.log(values);
+                    resetForm();
+                  }}
+                >
+                  {(formik) => {
+                    return (
+                      <Form action="#">
+                        <div>
+                          <label htmlFor="comment" className="sr-only">
+                            About
+                          </label>
+                          <div className="mt-1">
+                            <Field
+                              as="textarea"
+                              autoComplete="off"
+                              name="description"
+                              rows={3}
+                              className={` block w-full rounded-md border-2 bg-inherit p-2 shadow-sm outline-none focus:border-cyan-500 focus:ring-cyan-500 sm:text-sm`}
+                            />
+                          </div>
+                          {/* 
+			  input styles for form error
+			 ${
+                                formik.errors.description &&
+                                formik.touched.description
+                                  ? "border-red-500"
+                                  : null
+                              }
+			 
+			      error message display
+			  {formik.errors.description &&
+                            formik.touched.description && (
+                              <ErrMessage message={formik.errors.description} />
+                            )} */}
+                          <p className="mt-2 text-sm text-gray-500">
+                            Write a few sentences about your recipe.
+                          </p>
+                        </div>
+                        <div className="mt-3 flex items-center justify-end">
+                          <button
+                            type="submit"
+                            className="inline-flex items-center justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+                          >
+                            Comment
+                          </button>
+                        </div>
+                      </Form>
+                    );
+                  }}
+                </Formik>
+              </div>
             </div>
           </div>
-        </div>
+        )}
       </div>
     </section>
   );
