4795b81fcf91e65dbb6589e5aed8e7bdd690ffe7
Castle <castlerock88@protonmail.com>
2022-10-27 22:11:20 -0400

    refactor: removed routes import

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,14 +1,10 @@
 // src/server/trpc/router/_app.ts
 import { router } from "../trpc";
-import { exampleRouter } from "./example";
-import { authRouter } from "./auth";
 import { profileRouter } from "./profile";
 import { recipeRouter } from "./recipe";
 import { categoryRouter } from "./category";
 
 export const appRouter = router({
-  example: exampleRouter,
-  auth: authRouter,
   profile: profileRouter,
   recipe: recipeRouter,
   category: categoryRouter,
