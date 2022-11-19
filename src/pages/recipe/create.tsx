6a56e8df259a5bef8fc8b0c3da4362d05c3f4f16
Castle <castlerock88@protonmail.com>
2022-11-19 17:38:08 -0500

    refactor: create recipe meta head

--- src/pages/recipe/create.tsx
+++ src/pages/recipe/create.tsx
@@ -1,6 +1,7 @@
 // Packages
 import { GetServerSidePropsContext } from "next";
 import { getSession } from "next-auth/react";
+import Head from "next/head";
 import React from "react";
 
 // Components
@@ -8,9 +9,19 @@ import RecipeForm from "../../components/recipe/createForm/CreateRecipeForm";
 
 export default function create() {
   return (
-    <main className="px-2 py-4">
-      <RecipeForm />
-    </main>
+    <>
+      <Head>
+        <title>Create recipe</title>
+        <meta
+          name="description"
+          content={`Create a recipe to share with the world`}
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="px-2 py-4">
+        <RecipeForm />
+      </main>
+    </>
   );
 }
 
